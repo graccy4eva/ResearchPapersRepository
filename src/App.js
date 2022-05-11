f42816d42445623fd85734d16b0cb22e03e30507
Grace Ataguba <graccy4eva@gmail.com>
2022-05-11 19:57:38 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import faceid from './faceid.png';
+import './App.css';
+export default function App() {
+    return (
+        <div className="App">
+                <h1>Paper Repository</h1>
+        
+        </div>
+    );
+}
